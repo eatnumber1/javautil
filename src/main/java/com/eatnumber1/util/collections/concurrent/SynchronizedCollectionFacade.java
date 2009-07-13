9e2a9f1fd2a55a5d123b6444ca09534b7129426a
Russell Harmon <russ@eatnumber1.com>
2009-07-13 19:51:16 -0400

    Add copyright.

--- src/main/java/com/eatnumber1/util/collections/concurrent/SynchronizedCollectionFacade.java
+++ src/main/java/com/eatnumber1/util/collections/concurrent/SynchronizedCollectionFacade.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2007 Russell Harmon
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
 package com.eatnumber1.util.collections.concurrent;
 
 import com.eatnumber1.util.collections.concurrent.iterators.SynchronizedIterator;
@@ -10,6 +26,7 @@ import org.jetbrains.annotations.NotNull;
 
 /**
  * @author Russell Harmon
+ * @since Jul 13, 2009
  */
 public class SynchronizedCollectionFacade<T, D extends Collection<T>> extends SynchronizedReadWriteFacade<D> implements SynchronizedCollection<T, D> {
     public SynchronizedCollectionFacade( D delegate, @NotNull Lock lock ) {
